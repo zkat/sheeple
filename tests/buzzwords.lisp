f0b5bd1f1542e392c553c6c9ef80fa5079364ae4
Josh <sykopomp@sykosomatic.org>
2009-01-29 15:09:34 -0500

    More unit testing

--- tests/buzzwords.lisp
+++ tests/buzzwords.lisp
@@ -37,7 +37,11 @@
 errors when a buzzword doesn't exist."
   (defbuzzword test-buzz "This is a test")
   (is (buzzword-p (find-buzzword 'test-buzz)))
-  (signals no-such-buzzword (find-buzzword 'another-buzzword)))
+  (signals no-such-buzzword (find-buzzword 'another-buzzword))
+  (undefbuzzword test-buzz)
+  (defun test-buzz () (print "durr hurr"))
+  (signals clobbering-function-definition (defbuzzword test-buzz "OHNOES"))
+  (fmakunbound 'test-buzz))
 
 (test buzzword-undefinition
   "Usage of the undefbuzzword macro, confirmation of removal of all messages and
@@ -59,17 +63,56 @@ relevant role objects from the system."
   (signals no-such-buzzword (find-buzzword 'another-buzzer))
   (signals undefined-function (another-buzzer "WHAT ARE YOU GONNA DO, BLEED ON ME?!")))
 
+(test message-undefinition
+  "Tests undefmessage macro."
+  (defbuzzword test-message)
+  (defmessage foo (foo) foo)
+  (is (equal =dolly= (foo =dolly=)))
+  (undefmessage foo (foo))
+  (signals sheeple::no-most-specific-message (foo =dolly=))
+  (defmessage foo ((string =string=) (another =string=)) (concatenate 'string string another))
+  (is (equal "fullstring" (foo "full" "string")))
+  (undefmessage foo ((x =string=) (y =string=)))
+  (signals sheeple::no-most-specific-message (foo "full" "string"))
+  (undefbuzzword test-message))
+
 (test basic-message-definition
   "Checks that messages are defined properly, and added to their respective objects.
 also, checks that a style-warning is signaled if there is no buzzword defined."
   (undefbuzzword test-message)
   (signals style-warning (defmessage test-message (foo) (print foo)))
-  (undefbuzzword test-buzz)
   (defbuzzword test-message)
   (defmessage test-message (foo) (print foo))
   (is (buzzword-p (find-buzzword 'test-message)))
   (is (message-p (car (buzzword-messages (find-buzzword 'test-message)))))
-  (undefbuzzword test-message)
+  (undefbuzzword test-message))
+
+(test multimessage-definition
+  "Checks that multimessages are defined correctly, with roles added
+to their respective participants, with correct role-indexes, etc."
+  (defmessage test-message (foo bar) bar foo)
+  (let ((sheep1 (clone () () (:nickname "sheep1")))
+	(sheep2 (clone () () (:nickname "sheep2"))))
+    (defmessage test-message ((foo sheep1) (bar sheep2)) foo bar)
+    (is (equal t (sheeple::participant-p sheep1 'test-message)))
+    (is (equal t (sheeple::participant-p sheep2 'test-message)))
+    (let ((sheep1-roles (sheep-direct-roles sheep1))
+	  (sheep2-roles (sheep-direct-roles sheep2)))
+      (is (equal 0 (role-position (car sheep1-roles))))
+      (is (equal 1 (role-position (car sheep2-roles))))))
+  (undefbuzzword test-message))
+
+(test message-redefinition
+  "Confirms correct redefinition of messages"
+  (defbuzzword synergize)
+  (defmessage synergize ((x =number=) (y =number=)) (+ x y))
+  (is (= 10 (synergize 6 4)))
+  (defmessage synergize ((y =number=) (x =number=)) (* x y))
+  (is (= 24 (synergize 6 4)))
+  (undefbuzzword synergize))
+
+(test basic-message-dispatch
+  "Checks that basic single-dispatch messages work."
   (let ((test-sheep (clone () () (:nickname "testie")))
 	(another-sheep (clone () () (:nickname "rejected-failure"))))
     (defmessage test-message ((sheep test-sheep)) (sheep-nickname sheep))
@@ -77,9 +120,6 @@ also, checks that a style-warning is signaled if there is no buzzword defined."
     (signals sheeple::no-most-specific-message (test-message another-sheep)))
   (undefbuzzword test-message))
 
-(test multimessage-definition
-  "Checks that multimessages are defined correctly, with roles added
-to their respective participants, with correct role-indexes, etc."
-  (defmessage test-message (foo bar) (print foo) (print bar))
-  (undefbuzzword test-message)
+(test multimessage-dispatch
+  "Checks correct multimethod dispatching."
   )

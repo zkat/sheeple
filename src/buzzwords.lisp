7712cd957fc827e00ca7557d5f4ec2f1b6f11113
Kat Marchán <kzm@sykosomatic.org>
2009-02-06 01:27:25 -0500

    backend seems to support :before and :after. Adding frontend to test

--- src/buzzwords.lisp
+++ src/buzzwords.lisp
@@ -53,6 +53,10 @@
   ((name
     :initarg :name
     :accessor message-name)
+   (qualifiers
+    :initarg :qualifiers
+    :initform nil
+    :accessor message-qualifiers)
    (lambda-list
     :initarg :lambda-list
     :accessor message-lambda-list)
@@ -148,7 +152,7 @@
 	buzzword)))
 
 ;;; Message definition
-(defun ensure-message (&key name lambda-list participants function body)
+(defun ensure-message (&key name qualifiers lambda-list participants function body)
   (when (not (find-buzzword name nil))
     (progn
       (warn 'style-warning)
@@ -157,6 +161,7 @@
   (let* ((target-sheeple (sheepify-list participants))
 	 (message (make-instance 'standard-message
 				 :name name
+				 :qualifiers qualifiers
 				 :lambda-list lambda-list
 				 :participants participants
 				 :body body
@@ -257,43 +262,45 @@
 ;;; Message dispatch
 ;;;
 
-(defun apply-buzzword (selector args)
-  (let ((function (message-function (find-most-specific-message selector (sheepify-list args)))))
-    (apply function args)))
+(defun primary-message-p (message)
+  (null (message-qualifiers message)))
 
-(defun find-most-specific-message (selector args &optional not-this-message-please)
-  "Returns the most specific message using SELECTOR and ARGS."
-  (let ((n (length args))
-	(most-specific-message nil))
-    (loop 
-       for arg in args
-       for index upto (1- n)
-       do (let ((curr-sheep-list (sheep-hierarchy-list arg)))
-	    (loop
-	       for curr-sheep in curr-sheep-list
-	       for hierarchy-position upto (1- (length curr-sheep-list))
-	       do (dolist (role (sheep-direct-roles curr-sheep))
-		    (when (and (equal selector (role-name role))
-			       (eql index (role-position role)))
-			  (let ((curr-message (message-pointer role)))
-			    (when (= n (length (message-lambda-list curr-message)))
-			      (maybe-add-message-to-table curr-message)
-			      (setf (elt (message-rank curr-message) index) hierarchy-position)
-			      (when (fully-specified-p (message-rank curr-message)) 
-				(when (or (not most-specific-message)
-					  (eql not-this-message-please most-specific-message)
-					  (< (calculate-rank-score (message-rank curr-message))
-					     (calculate-rank-score (message-rank most-specific-message))))
-				  (setf most-specific-message curr-message))))))))))
-    (reset-message-ranks)
-    (if most-specific-message
-	most-specific-message
-	(error 'no-most-specific-message))))
+(defun before-message-p (message)
+  (when (member :before (message-qualifiers message))
+    t))
+
+(defun after-message-p (message)
+  (when (member :after (message-qualifiers message))
+    t))
+
+(defun apply-buzzword (selector args)
+  (let ((messages (find-applicable-messages selector
+					    (sheepify-list args))))
+    (apply-messages messages args)))
+
+(defun apply-messages (messages args)
+  (let ((primaries (remove-if-not #'primary-message-p messages))
+	(befores (remove-if-not #'before-message-p messages))
+	(afters (remove-if-not #'after-message-p messages)))
+    (when (null primaries)
+      (error "No primary messages"))
+    (dolist (before befores)
+      (apply-message before args nil))
+    (multiple-value-prog1
+	(apply-message (car primaries) args (cdr primaries))
+      (dolist (after (reverse afters))
+	(apply-message after args nil)))))
+
+(defun apply-message (message args next-messages)
+  (declare (ignore next-messages))
+  (let ((function (message-function message)))
+    (apply function args)))
 
 (defun find-applicable-messages  (selector args)
   "Returns the most specific message using SELECTOR and ARGS."
   (let ((n (length args))
-	(applicable-messages nil))
+	(discovered-messages nil)
+	(contained-applicable-messages nil))
     (loop 
        for arg in args
        for index upto (1- n)
@@ -306,14 +313,36 @@
 			       (eql index (role-position role)))
 			  (let ((curr-message (message-pointer role)))
 			    (when (= n (length (message-lambda-list curr-message)))
-			      (maybe-add-message-to-table curr-message)
-			      (setf (elt (message-rank curr-message) index) hierarchy-position)
-			      (when (fully-specified-p (message-rank curr-message))
-				(pushnew curr-message applicable-messages)))))))))
-    (reset-message-ranks)
-    (if applicable-messages
-	applicable-messages
+			      (when (not (member curr-message
+						 discovered-messages
+						 :key #'message-container-message))
+				(pushnew (contain-message curr-message) discovered-messages))
+			      (let ((contained-message (find curr-message
+							     discovered-messages
+							     :key #'message-container-message)))
+				(setf (elt (message-container-rank contained-message) index) 
+				      hierarchy-position)
+				(when (fully-specified-p (message-container-rank contained-message))
+				  (pushnew contained-message contained-applicable-messages :test #'equalp))))))))))
+    (if contained-applicable-messages
+	(sort-applicable-messages contained-applicable-messages)
 	(error 'no-applicable-messages))))
+
+(defun sort-applicable-messages (message-list &key (rank-key #'<))
+  (mapcar #'message-container-message 
+	  (sort message-list rank-key
+		:key (lambda (contained-message)
+		       (calculate-rank-score (message-container-rank contained-message))))))
+
+(defun contain-message (message)
+  (make-message-container
+   :message message
+   :rank (make-array (length (message-lambda-list message))
+		     :initial-element nil)))
+
+(defstruct message-container
+  message
+  rank)
 
 (define-condition no-applicable-messages (sheeple-error) ())
 (define-condition no-most-specific-message (sheeple-error) ())
@@ -330,27 +359,6 @@
        do (when (numberp item)
 	    (incf total item)))
     total))
-
-;; Message table
-(let ((message-table (make-hash-table :test #'equal)))
-
-  (defun maybe-add-message-to-table (message)
-    (unless (gethash message message-table)
-      (add-message-to-table message)))
-  
-  (defun add-message-to-table (message)
-    (setf (gethash message message-table) 
-	  (make-array (length (message-lambda-list message))
-		      :initial-element nil)))
-  
-  (defun message-rank (message)
-    (gethash message message-table))
-
-  (defun reset-message-ranks ()
-    (clrhash message-table))
-      
-  ) ; end message table closure
-
 
 
 

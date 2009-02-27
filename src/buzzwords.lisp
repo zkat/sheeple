498708cf4aca0f1b417e2f81b206def622bd4851
Kat Marchán <kzm@sykosomatic.org>
2009-02-26 20:28:21 -0500

    Switched apply-buzzword to use the buzzword, also:
    Got rid of make-buzzword-lambda. There's no way to make a lambda that will
    account for everything. If something is to be shown in the minibuffer, it
    has to be done by making the IDE check that the function being called is a
    buzzword and then have it inspect buzzword-lambda-list

--- src/buzzwords.lisp
+++ src/buzzwords.lisp
@@ -36,6 +36,11 @@
 (defun (setf buzzword-name) (new-value buzzword)
   (setf (property-value buzzword 'name) new-value))
 
+(defun buzzword-lambda-list (buzzword)
+  (property-value buzzword 'lambda-list))
+(defun (setf buzzword-lambda-list) (new-value buzzword)
+  (setf (property-value buzzword 'lambda-list) new-value))
+
 (defun buzzword-message-metasheep (buzzword)
   (property-value buzzword 'message-metasheep))
 (defun (setf buzzword-message-metasheep) (new-value buzzword)
@@ -86,18 +91,7 @@
 	(fn (std-make-buzzword-lambda buzzword)))
     (when (fboundp name)
      (warn 'clobbering-function-definition))
-    (setf (fdefinition name) fn)))
-
-;; TODO - this is gonna be pretty tricky ;|
-(defun std-make-buzzword-lambda (buzzword)
-  (let* ((lambda-list (buzzword-lambda-list buzzword))
-	 (requireds (extract-requireds lambda-list))
-	 (others (extract-other-args lambda-list)))
-    (eval `(lambda ,lambda-list (apply-buzzword buzzword requireds others)))))
-
-(defun extract-arguments (lambda-list)
-  ;...
-  )
+    (setf (fdefinition name) (apply-buzzword buzzword args))))
 
 (define-condition clobbering-function-definition (warning) ())
 (defun generate-sheep-standard-buzzword (metasheep 
@@ -158,94 +152,3 @@
 (defmacro undefbuzzword (name)
   `(undefine-buzzword
     ',name))
-
-;;; Yoinked from closette...
-;;; Several tedious functions for analyzing lambda lists
-
-(defun required-portion (gf args)
-  (let ((number-required (length (gf-required-arglist gf))))
-    (when (< (length args) number-required)
-      (error "Too few arguments to generic function ~S." gf))
-    (subseq args 0 number-required)))
-
-(defun gf-required-arglist (gf)
-  (let ((plist
-          (analyze-lambda-list 
-            (generic-function-lambda-list gf))))
-    (getf plist ':required-args)))
-
-(defun extract-lambda-list (specialized-lambda-list)
-  (let* ((plist (analyze-lambda-list specialized-lambda-list))
-         (requireds (getf plist ':required-names))
-         (rv (getf plist ':rest-var))
-         (ks (getf plist ':key-args))
-         (aok (getf plist ':allow-other-keys))
-         (opts (getf plist ':optional-args))
-         (auxs (getf plist ':auxiliary-args)))
-    `(,@requireds 
-      ,@(if rv `(&rest ,rv) ())
-      ,@(if (or ks aok) `(&key ,@ks) ())
-      ,@(if aok '(&allow-other-keys) ())
-      ,@(if opts `(&optional ,@opts) ())
-      ,@(if auxs `(&aux ,@auxs) ()))))
-
-(defun extract-specializers (specialized-lambda-list)
-  (let ((plist (analyze-lambda-list specialized-lambda-list)))
-    (getf plist ':specializers)))
-
-(defun analyze-lambda-list (lambda-list)
-  (labels ((make-keyword (symbol)
-              (intern (symbol-name symbol)
-                      (find-package 'keyword)))
-           (get-keyword-from-arg (arg)
-              (if (listp arg)
-                  (if (listp (car arg))
-                      (caar arg)
-                      (make-keyword (car arg)))
-                  (make-keyword arg))))
-    (let ((keys ())           ; Just the keywords
-          (key-args ())       ; Keywords argument specs
-          (required-names ()) ; Just the variable names
-          (required-args ())  ; Variable names & specializers
-          (specializers ())   ; Just the specializers
-          (rest-var nil)
-          (optionals ())
-          (auxs ())
-          (allow-other-keys nil)
-          (state :parsing-required))
-      (dolist (arg lambda-list)
-        (if (member arg lambda-list-keywords)
-	    (ecase arg
-	      (&optional
-	       (setq state :parsing-optional))
-	      (&rest
-	       (setq state :parsing-rest))
-	      (&key
-	       (setq state :parsing-key))
-	      (&allow-other-keys
-	       (setq allow-other-keys 't))
-	      (&aux
-	       (setq state :parsing-aux)))
-	    (case state
-	      (:parsing-required 
-	       (push-on-end arg required-args)
-	       (if (listp arg)
-		   (progn (push-on-end (car arg) required-names)
-			  (push-on-end (cadr arg) specializers))
-		   (progn (push-on-end arg required-names)
-			  (push-on-end 't specializers))))
-	      (:parsing-optional (push-on-end arg optionals))
-	      (:parsing-rest (setq rest-var arg))
-	      (:parsing-key
-	       (push-on-end (get-keyword-from-arg arg) keys)
-	       (push-on-end arg key-args))
-	      (:parsing-aux (push-on-end arg auxs)))))
-      (list  :required-names required-names
-             :required-args required-args
-             :specializers specializers
-             :rest-var rest-var
-             :keywords keys
-             :key-args key-args
-             :auxiliary-args auxs
-             :optional-args optionals
-             :allow-other-keys allow-other-keys))))

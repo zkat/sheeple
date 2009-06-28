f38be21d4d48d7103fa45dce81cf6cd4ff525149
Josh <sykopomp@sykosomatic.org>
2009-06-27 23:07:39 -0500

    Snip

--- tests/compatibility.lisp
+++ tests/compatibility.lisp
@@ -21,19 +21,18 @@
   (is (equal (find-proto 'cons) (box-type-of (cons 1 2))))
   (is (equal (find-proto 'character) (box-type-of #\a)))
   (is (equal (find-proto 'hash-table) (box-type-of (make-hash-table))))
-  (is (equal (find-proto 'package) (box-type-of (find-package :sheeple-tests))))
+  (is (equal (find-proto 'package) (box-type-of (find-package :sheeple))))
   (is (equal (find-proto 'pathname) (box-type-of #P"compatibility.lisp")))
   ;;  (is (equal (find-proto 'readtable) (box-type-of ???))) ; uhhh??
-  ;;  (is (equal (find-proto 'stream) (box-type-of ???))) ; dunno
+  (is (equal (find-proto 'stream) (box-type-of *standard-output*)))
   (is (equal (find-proto 'number) (box-type-of 1/2)))
   (is (equal (find-proto 'string) (box-type-of "foo")))
   (is (equal (find-proto 'bit-vector) (box-type-of #*)))
   (is (equal (find-proto 'vector) (box-type-of (vector 1 2 3))))
   (is (equal (find-proto 'array) (box-type-of (make-array '(1 2)))))
-;;  (is (equal =sequence= (box-type-of ???))) ;hm
   (is (equal (find-proto 'function) (box-type-of (lambda () 1)))))
 
 ;; (test clos-fleecing
-;;   "These will all fail for now."
+;; TODO!
 ;;   (defclass foo () ())
 ;;   (is (not (equal =white-fang= (fleece-of (make-instance 'foo))))))
--- tests/sheeple.lisp
+++ tests/sheeple.lisp
@@ -16,7 +16,19 @@
 (defun run-all-tests ()
   (run! 'sheeple))
 (defmethod asdf:perform ((o asdf:test-op) (c (eql (asdf:find-system :sheeple-tests))))
-  (run-all-tests))
+  (format t "~&~%*******************~%~
+                 ** Starting test **~%~
+                 *******************~%")
+  (run-all-tests)
+  (format t "~&*****************************************~%~
+               **            Tests finished           **~%~
+               *****************************************~%~
+               ** If there were any failures on your  **~%~
+               ** platform, please report them to me: **~%~
+               **  (sykopomp at sykosomatic dot org)  **~%~
+               ** or just file a bugreport on github: **~%~
+               ** github.com/sykopomp/sheeple/issues  **~%~
+               *****************************************~%"))
 
 (def-suite cloning :in sheeple)
 

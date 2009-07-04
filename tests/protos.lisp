469b1a44f23b50e42b779bf9e1ac3a2afb4467b0
Josh <sykopomp@sykosomatic.org>
2009-07-03 20:00:46 -0500

    Getting rid of tests/protos.lisp

--- tests/protos.lisp
+++
@@ -1,9 +0,0 @@
-;; This file is part of Sheeple.
-
-;; tests/protos.lisp
-;;
-;; Unit tests for src/protos.lisp
-;;
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-(in-package :sheeple)
-

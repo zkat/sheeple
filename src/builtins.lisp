16dd43c6f39eee790f6bf04ccd4eb3d23f31b7f5
Kat Marchán <kzm@sykosomatic.org>
2009-08-04 02:10:17 -0400

    Moved boxed-object-table into a defvar.

--- src/builtins.lisp
+++ src/builtins.lisp
@@ -57,34 +57,31 @@
 	(function                                      =function=)
 	(t                                             =boxed-object=))))
 
-;; Boxed object table
-(let ((boxed-object-table (make-hash-table :test #'equal)))
+(defvar *boxed-object-table* (make-hash-table :test #'equal))
 
-  (defun find-boxed-object (object &optional (errorp nil))
-    "Finds a previously-boxed object in the boxed object table.
+(defun find-boxed-object (object &optional (errorp nil))
+  "Finds a previously-boxed object in the boxed object table.
 If ERRORP is T, this signals an error if OBJECT is a sheep, or if OBJECT
 has not already been boxed."
-    (if (sheep-p object)
-        (if errorp (error "~S seems to already be a sheep." object) nil)
-        (multiple-value-bind (sheep hasp)
-            (gethash object boxed-object-table)
-          (if hasp
-              sheep
-              (if errorp (error "~S has not been boxed." object) nil)))))
+  (if (sheep-p object)
+      (if errorp (error "~S seems to already be a sheep." object) nil)
+      (multiple-value-bind (sheep hasp)
+          (gethash object *boxed-object-table*)
+        (if hasp
+            sheep
+            (if errorp (error "~S has not been boxed." object) nil)))))
 
-  (defun box-object (object)
-    "Wraps OBJECT with a sheep."
-    (if (sheep-p object)
-        (error "~S seems to already be a sheep." object)
-        (setf (gethash object boxed-object-table) 
-              (defclone ((box-type-of object))
-                  ((wrapped-object object)) (:nickname object)))))
+(defun box-object (object)
+  "Wraps OBJECT with a sheep."
+  (if (sheep-p object)
+      (error "~S seems to already be a sheep." object)
+      (setf (gethash object *boxed-object-table*) 
+            (defclone ((box-type-of object))
+                ((wrapped-object object)) (:nickname object)))))
 
-  (defun remove-boxed-object (object)
-    "Kills object dead"
-    (remhash object boxed-object-table))
-    
-  ) ; end boxed object table
+(defun remove-boxed-object (object)
+  "Kills object dead"
+  (remhash object *boxed-object-table*))
 
 (defun sheepify-list (obj-list)
   "Converts OBJ-LIST to a list where each item is either a sheep or a fleeced wolf."

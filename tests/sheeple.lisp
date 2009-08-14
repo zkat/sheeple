0e4a37c9644f3a4b3ce6d5ebc4eb586426f7de4d
Adlai <munchking@gmail.com>
2009-08-14 21:35:50 +0300

    Look ma, no test failures!

--- tests/sheeple.lisp
+++ tests/sheeple.lisp
@@ -9,6 +9,14 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (in-package :sheeple)
 
+(in-suite sheeple)
+
+;;; Secures sanity of pre-bootstrap development in a "fire-and-forget" fashion.
+(test bootstrappedp
+  (if *bootstrappedp*
+      (pass "Sheeple has bootstrapped")
+      (skip "Sheeple has not yet bootstrapped")))
+
 ;;;
 ;;; Allocation
 ;;;
@@ -463,7 +471,7 @@
 (def-suite clone-general :in cloning)
 (in-suite clone-general)
 
-(test ensure-sheep
+(test (ensure-sheep :depends-on bootstrappedp)
   ;; basic
   (let ((sheep (ensure-sheep nil)))
     (is (sheepp sheep))
@@ -491,7 +499,7 @@
     (is (eql test-metasheep (sheep-metasheep sheep)))
     (is (eql =standard-sheep= (car (sheep-parents sheep))))))
 
-(test clone
+(test (clone :depends-on bootstrappedp)
   (is (eql =standard-metasheep= (sheep-metasheep (clone))))
   (is (sheepp (clone)))
   (is (std-sheep-p (clone)))
@@ -513,13 +521,13 @@
     (is (parentp o3 sheep))
     (is (parentp o4 sheep))))
 
-(test sheep-nickname
+(test (sheep-nickname :depends-on bootstrappedp)
   (let ((sheep (clone)))
     (setf (sheep-nickname sheep) 'test)
     (is (eq 'test (sheep-nickname sheep)))
     (is (eq 'test (sheep-nickname (clone sheep))))))
 
-(test sheep-documentation
+(test (sheep-documentation :depends-on bootstrappedp)
   (let ((sheep (clone)))
     (setf (sheep-documentation sheep) 'test)
     (is (eq 'test (sheep-documentation sheep)))
@@ -563,7 +571,7 @@
   (is (equal '(:metasheep foo :other-option 'bar)
              (canonize-clone-options '((:metasheep foo) (:other-option 'bar))))))
 
-(test defclone
+(test (defclone :depends-on bootstrappedp)
   (let* ((parent (clone))
          (test-sheep (defclone (parent) ((var "value")))))
     (is (sheepp test-sheep))
@@ -578,7 +586,7 @@
 (def-suite protos :in cloning)
 (in-suite protos)
 
-(test defproto
+(test (defproto :depends-on bootstrappedp)
   (let ((test-proto (defproto =test-proto= () ((var "value")))))
     (is (sheepp test-proto))
     (is (eql test-proto =test-proto=))

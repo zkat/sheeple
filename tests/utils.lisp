d3b847ebbdce014466fc78db8cfa9ea40cc1309e
Adlai <munchking@gmail.com>
2009-08-16 17:04:29 +0300

    Test and documentation for #'proper-list-of-length-p
    This function is only used once though, in src/parse-lambda-list.lisp
    as an assertion in the macro COLLECT. Maybe we can do without?

--- tests/utils.lisp
+++ tests/utils.lisp
@@ -18,7 +18,15 @@
   (is (equal '(a b c) (flatten '((a) (b) (c)))))
   (is (equal '(a b c) (flatten '((a . b) . c)))))
 
-(test proper-list-of-length-p)
+(test proper-list-of-length-p
+  (signals type-error (proper-list-of-length-p 5 0))
+  (is (not (null (proper-list-of-length-p () 0))))
+  (is (null (proper-list-of-length-p '() 1)))
+  (is (null (proper-list-of-length-p '(1) 0)))
+  (is (not (null (proper-list-of-length-p '(1 2 3) 0 4))))
+  (is (null (proper-list-of-length-p '(1 2 3) 1 2)))
+  (5am:finishes (proper-list-of-length-p #1='(:P . #1#) 0 2)))
+
 (test topological-sort)
 (test once-only)
 (test memq)

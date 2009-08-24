f6fb905e2a20d2500f9fd0635777cd40c3421334
Josh <sykopomp@sykosomatic.org>
2009-08-24 11:32:51 -0400

    Moving reply stuff to separate files.

--- tests/messages.lisp
+++ tests/messages.lisp
@@ -1,5 +1,5 @@
 ;;;; -*- Mode: lisp; indent-tabs-mode: nil -*-
-;;;
+;;;;
 ;;;; This file is part of Sheeple
 
 ;;;; tests/messages.lisp
@@ -84,58 +84,3 @@
 (test check-msg-lambda-list)
 (test analyze-lambda-list)
 
-(def-suite reply-definition :in messages)
-(in-suite reply-definition)
-
-(test reply-p)
-(test role-p)
-(test participant-p)
-(test ensure-reply)
-(test generate-reply)
-(test defreply)
-(test create-msg-lambda-list)
-(test add-reply-to-message)
-(test remove-specific-reply)
-(test delete-reply)
-(test delete-role)
-(test add-reply-to-sheeple)
-(test undefine-reply)
-(test remove-applicable-reply)
-(test available-replies)
-(test add-readers-to-sheep)
-(test add-writers-to-sheep)
-(test make-reply-lambda)
-(test parse-defreply)
-(test extract-var-name)
-(test confirm-var-name)
-(test undefreply)
-(test parse-undefreply)
-
-(def-suite reply-dispatch :in messages)
-(in-suite reply-dispatch)
-
-(test primary-reply-p)
-(test before-reply-p)
-(test after-reply-p)
-(test around-reply-p)
-(test apply-message)
-(test apply-replies)
-(test next-reply-p)
-(test call-next-reply)
-(test compute-effective-reply-function)
-(test compute-primary-erfun)
-(test find-applicable-replies)
-(test %find-applicable-replies)
-;; caching
-(test create-reply-cache)
-(test desired-vector-entry-p)
-(test fetch-memo-vector-entry)
-(test add-entry-to-message)
-(test memoize-reply-dispatch)
-;; other dispatch stuff
-(test sort-applicable-replies)
-(test contain-reply)
-(test unbox-replies)
-(test fully-specified-p)
-(test calculate-rank-score)
-(test reply-specialized-portion)

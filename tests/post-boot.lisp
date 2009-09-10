82cd0a9906d794e79f0f8568ab5d92ddbc6fe2e7
Josh <sykopomp@sykosomatic.org>
2009-09-09 23:38:59 -0500

    Adding tests/post-boot.lisp

---
+++ tests/post-boot.lisp
@@ -0,0 +1,11 @@
+;;;; -*- Mode: Lisp; Syntax: ANSI-Common-Lisp; Base: 10; indent-tabs-mode: nil -*-
+
+;;;; This file is part of Sheeple
+
+;;;; tests/post-boot.lisp
+;;;;
+;;;; Unit tests for src/post-boot.lisp
+;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(in-package :sheeple)
+

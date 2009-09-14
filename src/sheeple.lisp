777149b35e6ed816e401ba7713a490c0fbcb1997
Kat Marchán <kzm@sykosomatic.org>
2009-09-13 19:01:22 -0500

    This shouldn't fire pre-bootstrap

--- src/sheeple.lisp
+++ src/sheeple.lisp
@@ -388,7 +388,7 @@ will be used instead of SHEEP's metasheep, but SHEEP itself remains unchanged."
                    ,(canonize-sheeple sheeple)
                    :properties ,(canonize-properties properties t)
                    ,@(canonize-options options))))
-       (unless (has-direct-property-p sheep :nickname)
+       (unless (or (not *bootstrappedp*) (has-direct-property-p sheep :nickname))
          (setf (sheep-nickname sheep) ',name))
        (setf (symbol-value ',name) sheep))))
 

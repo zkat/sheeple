96bc844117ddfc72caabd41d5d89a8f3d7d7e439
Adlai Chandrasekhar <munchking@gmail.com>
2009-10-13 04:18:46 +0200

    MAPS: Added conditions. Blech.

--- src/mapped.lisp
+++ src/mapped.lisp
@@ -64,6 +64,41 @@
                    (:copier      %copy-object))
   mold property-values roles)
 
+;;; This condition framework is straight from conditions.lisp
+
+(define-condition mold-condition ()
+  ((format-control :initarg :format-control :reader mold-condition-format-control))
+  (:report (lambda (condition stream)
+             (apply #'format stream (mold-condition-format-control condition)))))
+
+(defmacro define-mold-condition (name super (&optional string &rest args)
+                                    &rest condition-options)
+  (let (reader-names)
+    `(define-condition ,name ,(ensure-list super)
+       ,(loop for arg in args for reader = (intern (format nil "~A-~A" name arg))
+           collect
+             `(,arg :initarg ,(intern (symbol-name arg) :keyword) :reader ,reader)
+           do (push reader reader-names))
+       (:report
+        (lambda (condition stream)
+          (funcall #'format stream (mold-condition-format-control condition)
+                   ,@(mapcar #'(lambda (reader) `(,reader condition))
+                             (nreverse reader-names)))))
+       (:default-initargs :format-control ,string
+         ,@(cdr (assoc :default-initargs condition-options)))
+       ,@(remove :default-initargs condition-options :key #'car))))
+
+(define-mold-condition mold-warning (mold-condition warning) ())
+(define-mold-condition mold-error (mold-condition error) ())
+
+;;; Now for an original condition:
+
+(define-mold-condition mold-collision mold-error
+  ("Can't link ~A, because doing so would conflict with the already-linked ~A."
+   new-mold collision-mold))
+
+;;; And now for some code:
+
 (defvar *maps* (make-hash-table :test 'equal))
 
 (defun tree-find-if (test tree &key (key #'identity))

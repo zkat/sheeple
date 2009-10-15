69012beedae7854a2bc787f8b697f54933464a66
Josh Marchán <sykopomp@sykosomatic.org>
2009-10-15 06:26:07 -0500

    TESTS: All property tests pass.

--- tests/properties.lisp
+++ tests/properties.lisp
@@ -14,74 +14,13 @@
 ;;;
 (def-suite properties :in sheeple)
 
-(def-suite internals :in properties)
-(in-suite internals)
-
-(test %add-property-cons
-  (let ((object (spawn))
-        (property
-         #+sheeple3.1 (spawn =standard-property=)
-         #-sheeple3.1 'test))
-    (is (null (%object-properties object)))
-    (is (eq object (%add-property-cons object property nil)))
-    (signals error (%add-property-cons object property nil))
-    (is (not (null (%object-properties object))))
-    (is (vectorp (%object-properties object)))
-    (is (find property (%object-properties object)
-              :key #+sheeple3.1(fun (property-name (car _)))
-              #-sheeple3.1 'car))))
-
-(test %get-property-cons
-  (let* ((object (spawn))
-         (property
-          #+sheeple3.1 (defobject (=standard-property=) ((property-name 'test)))
-          #-sheeple3.1 'test)
-         (other-property
-          #+sheeple3.1 (defobject (=standard-property=) ((property-name 'other-prop)))
-          #-sheeple3.1 'other-prop))
-    (is (null (%get-property-cons object property)))
-    (%add-property-cons object property 'value)
-    (is (consp (%get-property-cons object property)))
-    (is (null (%get-property-cons object other-property)))
-    (is (eq property (car (%get-property-cons object property))))
-    (is (eq 'value (cdr (%get-property-cons object property))))))
-
-(test %remove-property-cons
-  (let* ((object (spawn))
-         (property
-          #+sheeple3.1 (defobject (=standard-property=) ((property-name 'test)))
-          #-sheeple3.1 'test))
-    (%add-property-cons object property 'value)
-    (is (eq object (%remove-property-cons object 'test)))
-    (is (null (%get-property-cons object 'tests)))))
-
-(test %direct-property-value
-  (let* ((object (spawn))
-         (property
-          #+sheeple3.1 (defobject (=standard-property=) ((property-name 'test)))
-          #-sheeple3.1 'test))
-    (%add-property-cons object property 'value)
-    (is (eq 'value (%direct-property-value object 'test)))))
-
-#+sheeple3.1
-(test %direct-property-metaobject
-  (let* ((object (spawn))
-         (property
-          #+sheeple3.1 (defobject (=standard-property=) ((property-name 'test)))
-          #-sheeple3.1 'test))
-    (%add-property-cons object property 'value)
-    (is (eq 'new-value (setf (%direct-property-value object 'test) 'new-value)))
-    (is (eq 'new-value (%direct-property-value object 'test)))))
-
 (def-suite existential :in properties)
 (in-suite existential)
 
 (test has-direct-property-p
   (let* ((object (spawn))
-         (property
-          #+sheeple3.1 (defobject (=standard-property=) ((property-name 'test)))
-          #-sheeple3.1 'test))
-    (%add-property-cons object property 'value)
+         (property 'test))
+    (add-property object property 'value)
     (is (has-direct-property-p object 'test))
     (is (not (has-direct-property-p object 'something-else))))
   (let* ((a (spawn))
@@ -103,7 +42,7 @@
   (let ((object (spawn)))
     (is (eq object (add-property object 'test 'value)))
     (is (has-direct-property-p object 'test))
-    (is (eq 'value (%direct-property-value object 'test)))
+    (is (eq 'value (direct-property-value object 'test)))
     (signals error (add-property object "foo" "uh oh"))
     (is (not (has-direct-property-p object "foo")))
     ;; todo - check that the restart works properly.
@@ -125,7 +64,10 @@
     (is (eq object (remove-all-direct-properties object)))
     (is (not (or (has-direct-property-p object 'test1)
                  (has-direct-property-p object 'test2)
-                 (has-direct-property-p object 'test3))))))
+                 (has-direct-property-p object 'test3))))
+    (signals unbound-property (property-value object 'test1))
+    (signals unbound-property (property-value object 'test2))
+    (signals unbound-property (property-value object 'test3))))
 
 (def-suite values :in properties)
 (in-suite values)
@@ -183,33 +125,17 @@
     (is (null (property-owner parent 'some-other-property)))
     (signals unbound-property (property-owner parent 'some-other-property t))))
 
-#+sheeple3.1
-(test property-metaobject-p
-  (is (property-metaobject-p (spawn =standard-property=)))
-  (is (not (property-metaobject-p (spawn)))))
-
-#+sheeple3.1
-(test direct-property-metaobject
-  (let ((object (defobject () ((var 'value)))))
-    (is (property-metaobject-p (direct-property-metaobject object 'var)))
-    (signals unbound-property (direct-property-metaobject object 'durr))
-    (is (null (direct-property-metaobject object 'durr nil)))
-    (signals unbound-property (direct-property-metaobject object 'durr t))))
-
 (test object-direct-properties
   (let ((object (defobject () ((var1 'val) (var2 'val) (var3 'val)))))
     (is (= 3 (length (object-direct-properties object))))
-    (is (every #+sheeple3.1 #'property-metaobject-p
-               #-sheeple3.1 #'symbolp
-               (object-direct-properties object)))))
+    (is (every 'symbolp (object-direct-properties object)))))
 
 (test available-properties
   (let* ((a (defobject () ((var1 'val))))
          (b (defobject (a) ((var2 'val))))
          (c (defobject (b) ((var3 'val)))))
     (is (= 4 (length (available-properties c))))
-    (is (every #+sheeple3.1 #'property-metaobject-p
-               #-sheeple3.1 #'symbolp (available-properties c)))))
+    (is (every 'symbolp (available-properties c)))))
 
 ;; ugh. I don't want to write tests for these.
 (test property-summary)

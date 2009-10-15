a6b73bff11ee9ffe38834e8d1d26ae7388917c3c
Josh Marchán <sykopomp@sykosomatic.org>
2009-10-15 01:47:14 -0500

    Disabling documentation until issues with CLISP are resolved.

--- src/messages.lisp
+++ src/messages.lisp
@@ -252,7 +252,8 @@ more entries the cache will be able to hold, but the slower lookup will be.")
   (let ((message (%make-message :name name :lambda-list lambda-list)))
     (set-arg-info message :lambda-list lambda-list)
     (finalize-message message)
-    (setf (documentation message 'message) documentation)
+    ;; Documenting documentation out until we've gotten it to work
+    #+nil(setf (documentation message 'message) documentation)
     message))
 
 ;; Finalizing a message sets the function definition of the message to a

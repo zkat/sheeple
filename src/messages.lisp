33e21d11fec710ba41bb94f8a0cae69d40d1b616
Josh Marchán <sykopomp@sykosomatic.org>
2009-10-17 00:30:22 -0500

    COMPILATION: After repeated failures with EVAL-WHEN, I've taken all the damn forms out.

--- src/messages.lisp
+++ src/messages.lisp
@@ -275,11 +275,9 @@ more entries the cache will be able to hold, but the slower lookup will be.")
 ;; This pair just pretties up the options during macro expansion
 (defmacro defmessage (name lambda-list &rest options)
   `(progn
-     (eval-when (:compile-toplevel :execute)
-       (check-message-lambda-list ',lambda-list))
-     (eval-when (:compile-toplevel :load-toplevel :execute)
-       (ensure-message ',name :lambda-list ',lambda-list
-                       ,@(canonize-message-options options)))))
+     (check-message-lambda-list ',lambda-list)
+     (ensure-message ',name :lambda-list ',lambda-list
+                     ,@(canonize-message-options options))))
 
 (defun canonize-message-option (option)
   `(,(car option) ,(cadr option)))

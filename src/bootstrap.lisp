90b77c658efda44aa4c907331c727c1163f1ce94
Kat Marchán <kzm@sykosomatic.org>
2009-10-17 15:25:58 -0500

    BOOTSTRAP: Making the msg/reply definitions toplevel.

--- src/bootstrap.lisp
+++ src/bootstrap.lisp
@@ -26,49 +26,49 @@
         (let ((obj (std-allocate-object =standard-metaobject=)))
           (setf (%object-mold obj)
                 (ensure-mold (list =t=) nil))
-          obj))
+          obj)))
 
-  ;; We can define special messages now.
-  (defmessage allocate-object (metaobject)
-    (:documentation "Allocates a object object based on METAOBJECT."))
-  (defreply allocate-object ((object =standard-metaobject=))
-    (std-allocate-object object))
+;; We can define special messages now.
+(defmessage allocate-object (metaobject)
+  (:documentation "Allocates a object object based on METAOBJECT."))
+(defreply allocate-object ((object =standard-metaobject=))
+  (std-allocate-object object))
 
-  (defmessage shared-init (object &key &allow-other-keys)
-    (:documentation "Adds properties to OBJECT and performs general initialization tasks."))
-  (defreply shared-init (object &key properties nickname)
-    (dolist (property-spec properties)
-      (destructuring-bind (name value &rest keys) property-spec
-        (apply 'add-property object name value keys)))
-    (when nickname
-      (setf (object-nickname object) nickname))
-    object)
+(defmessage shared-init (object &key &allow-other-keys)
+  (:documentation "Adds properties to OBJECT and performs general initialization tasks."))
+(defreply shared-init (object &key properties nickname)
+  (dolist (property-spec properties)
+    (destructuring-bind (name value &rest keys) property-spec
+      (apply 'add-property object name value keys)))
+  (when nickname
+    (setf (object-nickname object) nickname))
+  object)
 
-  (defmessage init-object (object &key &allow-other-keys)
-    (:documentation "Performs 'once-only' initialization tasks on OBJECT."))
-  (defreply init-object (object &key parents properties nickname)
-    (shared-init object :properties properties :nickname nickname))
+(defmessage init-object (object &key &allow-other-keys)
+  (:documentation "Performs 'once-only' initialization tasks on OBJECT."))
+(defreply init-object (object &key properties nickname)
+  (shared-init object :properties properties :nickname nickname))
 
-  (defmessage reinit-object (object &key &allow-other-keys)
-    (:documentation "Resets parents and properties without changing OBJECT's identity."))
-  (defreply reinit-object (object &key parents documentation properties)
-    ;; first, reset the mold
-    (change-mold object (ensure-mold (if (null parents)
-                                         (list =standard-object=)
-                                         (objectify-list parents))
-                                     nil))
-    ;; Setting up the properties all over again.
-    (dolist (property-spec properties)
-      (destructuring-bind (name value &rest keys) property-spec
-        (apply 'add-property object name value keys)))
-    ;; Finally, set the documentation. If none is provided, it's set to NIL.
-    ;; It's important to note that documenting a object will keep a reference to it...
-    ;; At the same time, REINIT-OBJECT is only meant for protos, so it should be fine.
-    (when documentation
-      (setf (documentation object t) documentation))
-    object)
+(defmessage reinit-object (object &key &allow-other-keys)
+  (:documentation "Resets parents and properties without changing OBJECT's identity."))
+(defreply reinit-object (object &key parents documentation properties)
+  ;; first, reset the mold
+  (change-mold object (ensure-mold (if (null parents)
+                                       (list =standard-object=)
+                                       (objectify-list parents))
+                                   nil))
+  ;; Setting up the properties all over again.
+  (dolist (property-spec properties)
+    (destructuring-bind (name value &rest keys) property-spec
+      (apply 'add-property object name value keys)))
+  ;; Finally, set the documentation. If none is provided, it's set to NIL.
+  ;; It's important to note that documenting a object will keep a reference to it...
+  ;; At the same time, REINIT-OBJECT is only meant for protos, so it should be fine.
+  (when documentation
+    (setf (documentation object t) documentation))
+  object)
 
-  ;; Now we redefine =standard-object= and =standard-metaobject= normally.
+(unless *bootstrappedp*
   (defproto =standard-object= (=t=) ())
   (change-mold =standard-metaobject=
                (ensure-mold (list =t=) nil))
@@ -95,6 +95,5 @@
   (defproto =complex= (=number=) ())
   (defproto =integer= (=number=) ())
   (defproto =float= (=number=) ())
-
-  ;; Make sure that we don't bootstrap the same image twice.
+    
   (setf *bootstrappedp* t))

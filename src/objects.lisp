f5c96932bcbb10a7d55a62f693c4ebd6b5136586
Adlai Chandrasekhar <munchking@gmail.com>
2009-10-19 03:22:31 +0200

    MESSAGES: Disabled incomplete disatch caching

--- src/objects.lisp
+++ src/objects.lisp
@@ -96,8 +96,9 @@ Sheeple to use class-based optimizations yet keep its dynamic power."
                    (members   lineage-members)) lineage
     (setf hierarchy (compute-hierarchy parents))
     (maphash (lambda (member children)
-               (dolist (role (%object-roles member))
-                 (clear-dispatch-cache (role-message role)))
+               (declare (ignore member))
+               ;; (dolist (role (%object-roles member))
+               ;;   (clear-dispatch-cache (role-message role)))
                (mapcar 'trigger-hierarchy-recalculation children))
              members)))
 

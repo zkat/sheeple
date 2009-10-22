9c309c21a1c4ed5327c9046c0665b9abf69eb1c4
Adlai Chandrasekhar <munchking@gmail.com>
2009-10-22 08:45:17 +0200

    TESTS: New test COUNT-REQUIRED-PARAMETERS

--- tests/lambda-lists.lisp
+++ tests/lambda-lists.lisp
@@ -14,5 +14,15 @@
 
 (test parse-lambda-list-like-thing)
 (test parse-lambda-list)
+
+(test count-required-parameters
+  (is (= 0 (count-required-parameters nil)))
+  (is (= 3 (count-required-parameters '(a b c))))
+  (is (= 0 (count-required-parameters '(&optional d))))
+  (is (= 0 (count-required-parameters '(&rest f))))
+  (is (= 0 (count-required-parameters '(&key g))))
+  (is (= 0 (count-required-parameters '(&aux h))))
+  (is (= 2 (count-required-parameters '(i j &key k)))))
+
 (test parse-specialized-lambda-list)
 (test analyze-lambda-list)

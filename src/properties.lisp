babe07e727e867b93d644903d4cb9daa580e6b75
Josh Marchán <sykopomp@sykosomatic.org>
2009-10-23 16:04:53 -0500

    PROPERTIES: New interface for adding properties. Let's try this out.

--- src/properties.lisp
+++ src/properties.lisp
@@ -31,27 +31,6 @@ would yield a value (i.e. not signal an unbound-property condition)."
   (some (rcurry 'has-direct-property-p property-name)
         (object-hierarchy-list object)))
 
-(defun add-property (object property-name value
-                     &key (reader nil readerp) (writer nil writerp) accessor)
-  "Adds a property named PROPERTY-NAME to OBJECT, initialized with VALUE."
-  (prog1 object
-    (assert (symbolp property-name))
-    (when (has-direct-property-p object property-name)
-      (cerror "Remove existing property." "~A already has a direct property named ~A."
-              object property-name)
-      (remove-property object property-name))
-    (change-mold object (ensure-transition (%object-mold object) property-name))
-    (let ((position (property-position property-name object)))
-      (setf (svref (%object-property-values object) position) value))
-    (when reader (add-reader-to-object reader property-name object))
-    (when writer (add-writer-to-object writer property-name object))
-    (when accessor
-      (let ((accessor-name (if (eq t accessor) property-name accessor)))
-        (unless (and readerp (null reader))
-          (add-reader-to-object accessor-name property-name object))
-        (unless (and writerp (null writer))
-          (add-writer-to-object `(setf ,accessor-name) property-name object))))))
-
 (defun remove-property (object property-name)
   "Removes OBJECT's direct property named PROPERTY-NAME. Signals an error if there is no such
 direct property. Returns OBJECT."
@@ -95,22 +74,25 @@ a condition of type UNBOUND-PROPERTY is signaled."
             (svref (%object-property-values it) index)))
          (t (error 'unbound-property :object object :property-name property-name))))
 
-(defun (setf property-value) (new-value object property-name)
+(defun (setf property-value) (new-value object property-name
+                              &key (reader nil readerp) (writer nil writerp) accessor)
   "Sets NEW-VALUE as the value of a direct-property belonging to OBJECT, named
-PROPERTY-NAME. If the property does not already exist anywhere in the hierarchy list, an error
-is signaled."
-  (acond ((property-position property-name object)
-          (setf (svref (%object-property-values object) it) new-value))
-         ((loop for ancestor in (mold-hierarchy (%object-mold object))
-             when (property-position property-name ancestor)
-             return ancestor)
-          (change-mold object (ensure-transition (%object-mold object) property-name))
-          (let ((index (property-position property-name object)))
-            (setf (svref (%object-property-values object) index) new-value)))
-         (t (cerror "Add the property locally" 'unbound-property
-                    :object object
-                    :property-name property-name)
-            (add-property object property-name new-value)))
+PROPERTY-NAME."
+  (assert (symbolp property-name))
+  (aif (property-position property-name object)
+       (setf (svref (%object-property-values object) it) new-value)
+       (progn
+         (change-mold object (ensure-transition (%object-mold object) property-name))
+         (let ((index (property-position property-name object)))
+           (setf (svref (%object-property-values object) index) new-value))))
+  (when reader (add-reader-to-object reader property-name object))
+  (when writer (add-writer-to-object writer property-name object))
+  (when accessor
+    (let ((accessor-name (if (eq t accessor) property-name accessor)))
+      (unless (and readerp (null reader))
+        (add-reader-to-object accessor-name property-name object))
+      (unless (and writerp (null writer))
+        (add-writer-to-object `(setf ,accessor-name) property-name object))))
   new-value)
 
 ;;; Object Documentation

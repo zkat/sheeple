dc340354967fe4da2f6fa29020e588e418840d1d
Adlai Chandrasekhar <munchking@gmail.com>
2009-10-31 05:45:17 +0200

    Revert "DEFPROTO: Evaluating defproto at compile-time..."
    This reverts commit a2b988b958ea8d5c513aac318225c0bb1ad284b4.

--- src/objects.lisp
+++ src/objects.lisp
@@ -439,7 +439,7 @@ will be used instead of OBJECT's metaobject, but OBJECT itself remains unchanged
 
 (defmacro defproto (name objects properties &rest options)
   "Words cannot express how useful this is."
-  `(eval-when (:compile-toplevel :load-toplevel :execute)
+  `(progn
      (declaim (special ,name))
      (setf (symbol-value ',name)
            (ensure-object (when (boundp ',name)

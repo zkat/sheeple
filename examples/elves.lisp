89e8f1c6adb1e472303bb3f1da12c880c26c2a4b
Adlai Chandrasekhar <munchking@gmail.com>
2009-12-14 10:42:45 +0200

    DEMOS: Updating elves.lisp

--- examples/elves.lisp
+++ examples/elves.lisp
@@ -19,29 +19,30 @@
 
 ;; Now let's say we want to make these entities mate, and vary the results.
 (defmessage mate (a b)
-  (:documentation "Mates A and B, and returns their baby :)"))
-(defreply mate ((a =entity=) (b =entity=))
-  ;; this reply will only dispatch if the item in position 'a' is an =entity=, AND 'b'
-  ;; is also an =entity=. Otherwise, it will signal an error.
-  (when (eql a b)
-    (error "Can't mate two of the exact same entity!"))
-  ;; OBJECT and DEFPROTO return the same kind of object.
-  ;; The difference is that defproto forms can be re-evaluated to 'redefine' a prototype
-  ;; while maintaining its identity.
-  (object :parents (list a b))
-  )
+  (:documentation "Mates A and B, and returns their baby :)")
+  (:reply ((a =entity=) (b =entity=))
+    ;; this reply will only dispatch if the item in position 'a' is an =entity=, AND 'b'
+    ;; is also an =entity=. Otherwise, it will signal an error.
+    (when (eql a b)
+      (error "Can't mate two of the exact same entity!"))
+    ;; OBJECT and DEFPROTO return the same kind of object.
+    ;; The difference is that defproto forms can be re-evaluated to 'redefine' a prototype
+    ;; while maintaining its identity.
+    (object :parents (list a b))))
 
 ;; When OBJECT isn't enough, there's DEFOBJECT, which works just like DEFPROTO except the object
 ;; it creates is 'anonymous'. It also does not automatically create new accessors. You must
 ;; explicitly pass it the :accessor option in order to do that.
-(defvar *edmond* (defobject (=human=)
-                     ((name "Edmond"))
-                   (:nickname "Eddy"))) ; Nicknames are displayed when the object is printed.
+(defvar *edmond*
+  (defobject =human=
+      ((name "Edmond"))
+    :nickname "Eddy")) ; Nicknames are displayed when the object is printed.
 
-(defvar *princess-renee* (defobject (=elf=)
-                            ((name "Renee")
-                             (title "Princess" :accessor 'title))
-                          (:nickname "Reni")))
+(defvar *princess-renee*
+  (defobject =elf=
+      ((name "Renee")
+       (title "Princess" :accessor 'title))
+    :nickname "Reni"))
 
 ;; Let's give the child a name so it doesn't just end up taking its parent's name.
 (defreply mate :around ((a *edmond*) (b *princess-renee*))
@@ -52,9 +53,9 @@
     the-child))
 
 ;; Princess Renee actually has a title. Maybe we want to be able to access the full name of an entity.
-(defmessage full-name (entity))
-(defreply full-name ((entity =entity=))
-  (name entity))
+(defmessage full-name (entity)
+  (:reply ((entity =entity=))
+    (name entity)))
 ;; We might be tempted to do something like this, but maybe that's not the right thing to express...
 ;; (defreply full-name ((royalty *princess-renee*))
 ;;   (format nil "~a ~a" (title royalty) (name royalty)))

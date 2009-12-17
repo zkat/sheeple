d9e83c4b10f50ebff394c0da9f0d6c1e5e7599a4
Adlai Chandrasekhar <munchking@gmail.com>
2009-12-17 20:11:25 +0200

    PACKAGES: The export lists are easier to edit this way

--- src/packages.lisp
+++ src/packages.lisp
@@ -3,50 +3,114 @@
 (defpackage #:%sheeple-garbage
   (:use :cl)
   (:shadow :make-hash-table)
-  (:export #:gc #:weak-pointer
-           #:make-weak-pointer
-           #:weak-pointer-value
-           #:weak-pointer-p
-           #:maybe-make-weak-pointer
-           #:maybe-weak-pointer-value
-           #:make-weak-hash-table
-           #:hash-table-weakness
-           #:finalize
-           #:cancel-finalization))
+  (:export
+   #:gc
+   #:weak-pointer
+   #:make-weak-pointer
+   #:weak-pointer-value
+   #:weak-pointer-p
+   #:maybe-make-weak-pointer
+   #:maybe-weak-pointer-value
+   #:make-weak-hash-table
+   #:hash-table-weakness
+   #:finalize
+   #:cancel-finalization
+   ))
 
 (defpackage #:sheeple
   (:use :cl :%sheeple-garbage)
   (:export
 
    ;; Objects
-   #:object #:defobject #:defproto #:clone #:objectp #:object-parents #:ancestorp #:parentp
-   #:descendantp #:childp #:init-object #:shared-init #:reinit-object #:print-sheeple-object
-   #:object-nickname #:object-hierarchy-list #:create
+   #:object
+   #:defobject
+   #:defproto
+   #:clone
+   #:objectp
+   #:object-parents
+   #:ancestorp
+   #:parentp
+   #:descendantp
+   #:childp
+   #:init-object
+   #:shared-init
+   #:reinit-object
+   #:print-sheeple-object
+   #:object-nickname
+   #:object-hierarchy-list
+   #:create
 
    ;; Property access
-   #:property-value #:direct-property-value #:available-properties
-   #:remove-property #:property-makunbound #:remove-all-direct-properties
-   #:direct-property-p #:with-properties #:direct-properties
+   #:property-value
+   #:direct-property-value
+   #:available-properties
+   #:remove-property
+   #:property-makunbound
+   #:remove-all-direct-properties
+   #:direct-property-p
+   #:with-properties
+   #:direct-properties
 
    ;; Messages
-   #:defmessage #:defreply #:available-replies #:undefmessage #:undefreply #:participantp
-   #:call-next-reply #:next-reply-p
+   #:defmessage
+   #:defreply
+   #:available-replies
+   #:undefmessage
+   #:undefreply
+   #:participantp
+   #:call-next-reply
+   #:next-reply-p
 
    ;; Protos
-   #:=standard-metaobject= #:=standard-object= #:=t= #:=null= #:=symbol= #:=complex= #:=integer=
-   #:=float= #:=cons= #:=character= #:=hash-table= #:=package= #:=pathname= #:=readtable= #:=stream=
-   #:=number= #:=string= #:=bit-vector= #:=vector= #:=array= #:=function= #:=boxed-object=
+   #:=standard-metaobject=
+   #:=standard-object=
+   #:=t=
+   #:=null=
+   #:=symbol=
+   #:=complex=
+   #:=integer=
+   #:=float=
+   #:=cons=
+   #:=character=
+   #:=hash-table=
+   #:=package=
+   #:=pathname=
+   #:=readtable=
+   #:=stream=
+   #:=number=
+   #:=string=
+   #:=bit-vector=
+   #:=vector=
+   #:=array=
+   #:=function=
+   #:=boxed-object=
 
    ;; built-ins
-   #:box-type-of #:find-boxed-object #:objectify
+   #:box-type-of
+   #:find-boxed-object
+   #:objectify
 
    ;; Conditions
-   #:sheeple-error #:sheeple-warning #:topological-sort-conflict #:object-hierarchy-error
-   #:object-property-error #:unbound-property #:unbound-direct-property
-   #:clobbering-function-definition #:sheeple-message-error #:insufficient-message-args
-   #:no-such-message #:message-lambda-list-error #:sheeple-reply-error #:reply-argument-conflict
-   #:automatic-message-creation #:reply-lambda-list-conflict #:no-applicable-reply
-   #:no-primary-reply #:specialized-lambda-list-error #:no-next-reply
+   #:sheeple-error
+   #:sheeple-warning
+   #:topological-sort-conflict
+   #:object-hierarchy-error
+   #:object-property-error
+   #:unbound-property
+   #:unbound-direct-property
+   #:clobbering-function-definition
+   #:sheeple-message-error
+   #:insufficient-message-args
+   #:no-such-message
+   #:message-lambda-list-error
+   #:sheeple-reply-error
+   #:reply-argument-conflict
+   #:automatic-message-creation
+   #:reply-lambda-list-conflict
+   #:no-applicable-reply
+   #:no-primary-reply
+   #:specialized-lambda-list-error
+   #:no-next-reply
    ))
 
 (defpackage #:sheeple-metaobject-protocol
@@ -54,12 +118,19 @@
   (:nicknames :smop :sheeple-mop)
   (:export
    ;; Objects
-   #:allocate-object #:compute-object-hierarchy-list-using-metaobject
+   #:allocate-object
+   #:compute-object-hierarchy-list-using-metaobject
 
    ;; Properties
-   :direct-property-p :remove-property :remove-all-direct-properties
-   :direct-property-value :property-value :property-owner
-   :direct-properties :available-properties :property-makunbound
+   :direct-property-p
+   :remove-property
+   :remove-all-direct-properties
+   :direct-property-value
+   :property-value
+   :property-owner
+   :direct-properties
+   :available-properties
+   :property-makunbound
    :remove-all-direct-properties
    ))
 

9c191f0b1d17ef4d2a0e663c0478b39c45222225
Adlai Chandrasekhar <munchking@gmail.com>
2009-12-17 20:54:16 +0200

    BOOT: Factoring the boot into separate files

---
+++ src/boot/init.lisp
@@ -0,0 +1,39 @@
+;;;; -*- Mode: Lisp; Syntax: ANSI-Common-Lisp; Base: 10; indent-tabs-mode: nil -*-
+
+;;;; This file is part of Sheeple
+
+;;;; boot/init.lisp
+;;;;
+;;;; Defining the object initialization protocol
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(in-package :sheeple)
+
+(defmessage shared-init (object &rest initargs)
+  (:documentation "Adds properties to OBJECT and performs general initialization tasks.")
+  (:reply (object &key properties (documentation nil doxp) (nickname nil nicknamep))
+    (dolist (property-spec properties)
+      (destructuring-bind (name &optional value &rest keys) (ensure-list property-spec)
+        (apply #'(setf property-value) value object name keys)))
+    (when nicknamep
+      (setf (object-nickname object) nickname))
+    (when doxp
+      (setf (documentation object t) documentation))
+    object))
+
+(defmessage init-object (object &rest initargs)
+  (:documentation "Performs 'once-only' initialization tasks on OBJECT.")
+  (:reply (object &rest initargs &key)
+    (apply #'shared-init object initargs)))
+
+(defmessage reinit-object (object &rest initargs)
+  (:documentation "Resets parents and properties without changing OBJECT's identity.")
+  (:reply (object &rest initargs &key parents)
+    (when (null parents)                ; Guard against funny business
+      (push =standard-object= parents))
+    (change-mold object (ensure-mold parents))
+    (apply #'shared-init object initargs)))
+
+(defmessage create (proto &key)
+  (:documentation "Creates a PROTO. Intended for customization.")
+  (:reply (proto &rest properties)
+    (object :parents `(,proto) :properties (plist-to-wide-alist properties))))

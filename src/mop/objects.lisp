25808688b7310387247f3ed48f98d6f860c35673
Adlai Chandrasekhar <munchking@gmail.com>
2009-12-17 20:06:59 +0200

    MOP: New file mop/objects.lisp

---
+++ src/mop/objects.lisp
@@ -0,0 +1,18 @@
+;;;; -*- Mode: Lisp; Syntax: ANSI-Common-Lisp; Base: 10; indent-tabs-mode: nil -*-
+
+;;;; This file is part of Sheeple
+
+;;;; mop/objects.lisp
+;;;;
+;;;; Object creation, cloning, inspection
+;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(in-package :sheeple)
+
+(defmessage smop:allocate-object (metaobject)
+  (:reply ((metaobject =standard-metaobject=))
+    (std-allocate-object metaobject)))
+
+(defmessage smop:compute-object-hierarchy-list-using-metaobject (metaobject object)
+  (:reply ((metaobject =standard-metaobject=) object)
+    (std-compute-object-hierarchy-list object)))

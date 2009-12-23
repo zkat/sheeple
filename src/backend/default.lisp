d65e5348b53b000c9b9814583862eb8400369543
Adlai Chandrasekhar <munchking@gmail.com>
2009-12-24 00:22:10 +0200

    BACKEND: Adding empty src/backend/default.lisp

---
+++ src/backend/default.lisp
@@ -0,0 +1,9 @@
+;;;; -*- Mode: lisp; indent-tabs-mode: nil -*-
+;;;;
+;;;; This file is part of Sheeple
+
+;;;; backend/default.lisp
+;;;;
+;;;; Portable versions of backend-customizable bits
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(in-package :sheeple)

03750d71b32524504ca9509d59b4e14250b22dde
Josh <sykopomp@sykosomatic.org>
2009-12-26 16:19:29 -0600

    MOP: Updated SHEEPLE-MOP messages to use property metaobjects

--- src/mop/properties.lisp
+++ src/mop/properties.lisp
@@ -9,37 +9,37 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (in-package :sheeple)
 
-(defmessage smop:direct-property-value (metaobject object property-name)
-  (:reply ((metaobject =standard-metaobject=) object property-name)
-    (std-direct-property-value object property-name)))
+(defmessage smop:direct-property-value (metaobject object propd)
+  (:reply ((metaobject =standard-metaobject=) object (propd =standard-property=))
+    (std-direct-property-value object (property-name propd))))
 
-(defmessage smop:property-value (metaobject object property-name)
-  (:reply ((metaobject =standard-metaobject=) object property-name)
-    (std-property-value object property-name)))
+(defmessage smop:property-value (metaobject object propd)
+  (:reply ((metaobject =standard-metaobject=) object (propd =standard-property=))
+    (std-property-value object (property-name propd))))
 
 (defmessage smop:add-direct-property (metaobject object property-name &key)
   (:reply ((metaobject =standard-metaobject=) object property-name &rest options)
     (apply #'std-add-direct-property object property-name options)))
 
-(defmessage (setf smop:direct-property-value) (new-value metaobject object property-name &key)
-  (:reply (new-value (metaobject =standard-metaobject=) object property-name &rest options)
-    (apply #'(setf std-direct-property-value) new-value object property-name options)))
+(defmessage (setf smop:direct-property-value) (new-value metaobject object propd &key)
+  (:reply (new-value (metaobject =standard-metaobject=) object (propd =standard-property=) &rest options)
+    (apply #'(setf std-direct-property-value) new-value object (property-name propd) options)))
 
-(defmessage smop:property-makunbound (metaobject object property-name)
-  (:reply ((metaobject =standard-metaobject=) object property-name)
-    (std-property-makunbound object property-name)))
+(defmessage smop:property-makunbound (metaobject object propd)
+  (:reply ((metaobject =standard-metaobject=) object (propd =standard-property=))
+    (std-property-makunbound object (property-name propd))))
 
 (defmessage smop:remove-all-direct-properties (metaobject object)
   (:reply ((metaobject =standard-metaobject=) object)
     (std-remove-all-direct-properties object)))
 
-(defmessage smop:direct-property-p (metaobject object property-name)
-  (:reply ((metaobject =standard-metaobject=) object property-name)
-    (std-direct-property-p object property-name)))
+(defmessage smop:direct-property-p (metaobject object propd)
+  (:reply ((metaobject =standard-metaobject=) object (propd =standard-property=))
+    (std-direct-property-p object (property-name propd))))
 
-(defmessage smop:property-owner (metaobject object property-name)
-  (:reply ((metaobject =standard-metaobject=) object property-name)
-    (std-property-owner object property-name)))
+(defmessage smop:property-owner (metaobject object propd)
+  (:reply ((metaobject =standard-metaobject=) object (propd =standard-property=))
+    (std-property-owner object (property-name propd))))
 
 (defmessage smop:direct-properties (metaobject object)
   (:reply ((metaobject =standard-metaobject=) object)

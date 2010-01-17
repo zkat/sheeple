997706a87417b32ff635c7aba3304429d7438838
Adlai Chandrasekhar <munchking@gmail.com>
2010-01-17 07:46:47 +0200

    PORTS: Rewriting FEATURE-CASE forms

--- src/ports.lisp
+++ src/ports.lisp
@@ -10,39 +10,37 @@
 
 (declaim (inline safe-fdefinition))
 (defun safe-fdefinition (name)
-  #+ccl (fboundp name)
-  #-ccl (when (fboundp name) (fdefinition name)))
+  (feature-case
+      (when (fboundp name)
+        (fdefinition name))
+    #+ccl (fboundp name)))
 
 (declaim (inline copy-simple-vector))
 (defun copy-simple-vector (vector)
   (declare (simple-vector vector) (optimize speed))
-  #+ccl (ccl::copy-uvector vector)
-  #-ccl (make-array (length vector) :initial-contents vector))
+  (feature-case
+      (make-array (length vector) :initial-contents vector)
+    #+ccl (ccl::copy-uvector vector)))
 
-(feature-case
-    (defun vector-cons (x vector)
-      (declare (simple-vector vector) (optimize speed))
-      (aprog1 (make-array (1+ (length vector)))
-        (loop for elt across vector and i from 1
-           do (setf (svref it i) elt)
-           finally (setf (svref it 0) x)))))
+(defun vector-cons (x vector)
+  (declare (simple-vector vector) (optimize speed))
+  (aprog1 (make-array (1+ (length vector)))
+    (loop for elt across vector and i from 1
+       do (setf (svref it i) elt)
+       finally (setf (svref it 0) x))))
 
-(feature-case
-    (defun record-message-compilation (name lambda-list env)
-      ;; What should be the default way to note a message at compile time?
-      (declare (ignore lambda-list env))
+(defun record-message-compilation (name lambda-list env)
+  ;; What should be the default way to note a message at compile time?
+  (declare (ignore lambda-list env))
+  (feature-case
       `(proclaim `(ftype function ,',name))))
 
-(feature-case
-    (defun record-message-source (name)
-      (declare (ignore name)))
-  #+ccl
-  (defun record-message-source (name)
-    (ccl:record-source-file name 'message)))
+(defun record-message-source (name)
+  (declare (ignorable name))
+  (feature-case nil
+    #+ccl (ccl:record-source-file name 'message)))
 
-(feature-case
-    (defun record-message-arglist (name arglist)
-      (declare (ignore name arglist)))
-  #+ccl
-  (defun record-message-arglist (name arglist)
-    (ccl::record-arglist name arglist)))
+(defun record-message-arglist (name arglist)
+  (declare (ignorable name arglist))
+  (feature-case nil
+    #+ccl (ccl::record-arglist name arglist)))
